9765410afbd1dcac6097c16243f744a479de1037
Rodrigo <ra.ramirez1993@gmail.com>
2020-09-02 00:35:32 -0400

    4

---
+++ docs/precache-manifest.b9b759985cc6fafb08f41b717fb8cfa6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0ff8431ec8fec303838046093a7a6aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b8bc3060d8b3c4004f9",
+    "url": "/static/css/2.b423ba47.chunk.css"
+  },
+  {
+    "revision": "25805b83d6751870a9f9",
+    "url": "/static/css/main.c03f8d59.chunk.css"
+  },
+  {
+    "revision": "5b8bc3060d8b3c4004f9",
+    "url": "/static/js/2.92825fd1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.92825fd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25805b83d6751870a9f9",
+    "url": "/static/js/main.de806900.chunk.js"
+  },
+  {
+    "revision": "466c1a7fdaa58576fa1f",
+    "url": "/static/js/runtime-main.7d4b7ba4.js"
+  }
+]);
